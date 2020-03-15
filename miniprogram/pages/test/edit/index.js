1ff1d183fc7f15f6f9771880fb3a31b7b9447fcc
Scott Wan <oncwnuKMMQbK62AfJkKp1MmOX8cg@git.weixin.qq.com>
2020-03-15 15:00:38 +0800

    question edit, delete and list done

--- miniprogram/pages/test/edit/index.js
+++ miniprogram/pages/test/edit/index.js
@@ -15,11 +15,28 @@ Page({
   onLoad: function (options) {
 
     let courseId = options.courseId
-    let questionId = options.questionId ? options.questionId : `${Date.now()}-${Math.floor(Math.random()*10000)}`
+    let questionId = options.questionId ? options.questionId : `${Date.now()}-${Math.floor(Math.random() * 10000)}`
+
     this.setData({
       courseId,
-      questionId
+      questionId,
     })
+
+    if (options.questionId) {
+      wx.cloud.database().collection('test').doc(questionId).get().then(result => this.setData({ question: result.data }))
+      wx.cloud.database().collection('testAnswer').doc(questionId).get().then(result => {
+
+        let answerChecked = {}
+
+        result.data.answer.forEach(el => {
+          answerChecked[el] = true
+        })
+
+        this.setData({ answer: result.data.answer, answerChecked })
+      })
+    }
+
+
 
   },
 
@@ -74,19 +91,28 @@ Page({
 
   checkboxChange: function (e) {
     // console.log('checkbox发生change事件，携带value值为：', e.detail.value)
-    let answer =  e.detail.value
+    let answer = e.detail.value
+
+    // console.log('A:', answer.indexOf('A') >= 0)
+    // console.log('B:', answer.indexOf('B') >= 0)
+    // console.log('C:', answer.indexOf('C') >= 0)
+    // console.log('D:', answer.indexOf('D') >= 0)
 
     this.setData({
       answer
     })
   },
 
-  formSubmit(e){
+  formSubmit(e) {
     // console.log('form发生了submit事件，携带数据为：', e.detail.value)
     let data = e.detail.value
     data.courseId = this.data.courseId
 
-    wx.cloud.database().collection('test').doc(this.data.questionId).set({data})
-    wx.cloud.database().collection('testAnswer').doc(this.data.questionId).set({data: {answer: this.data.answer}})
+    wx.cloud.database().collection('test').doc(this.data.questionId).set({ data })
+    wx.cloud.database().collection('testAnswer').doc(this.data.questionId).set({ data: { answer: this.data.answer } })
+
+    wx.navigateBack({
+      complete: (res) => {},
+    })
   }
 })
---
+++ miniprogram/pages/test/list/index.js
@@ -0,0 +1,94 @@
+// miniprogram/pages/test/list/index.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+    let courseId = options.courseId
+
+    this.setData({
+      courseId
+    })
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+    // console.log('>>> onshow of test list page.')
+    let courseId = this.data.courseId
+    wx.cloud.database().collection('test').where({ courseId }).get().then(result => this.setData({ list: result.data }))
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  },
+
+  delete(e){
+    let index = e.mark.index
+    let question = this.data.list[index]
+    let _this = this
+    
+    wx.showModal({
+      cancelColor: 'cancelColor',
+      title: `Are you sure to delete this record # ${index + 1}?`,
+      success(res){
+        if(res.confirm){
+          wx.cloud.database().collection('test').doc(question._id).remove()
+          wx.cloud.database().collection('testAnswer').doc(question._id).remove()
+          _this.data.list.splice(index,1)
+
+          _this.setData({
+            list: _this.data.list
+          })
+        }
+      }
+    })
+  }
+})
